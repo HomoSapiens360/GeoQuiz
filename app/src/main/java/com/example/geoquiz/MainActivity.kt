343e8632977087e0325506a580d21c85da0fea5c
Vagif <vaga1al@mail.ru>
2022-11-06 22:46:46 +0500

    Блокирование возможности отвечать на один и тот же вопрос несколько раз. Вывод пользователю процент правильных ответов

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.geoquiz
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 //import android.view.Gravity
 import android.view.View
 import android.widget.Button
@@ -10,10 +11,11 @@ import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
+private const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var nextButton: ImageButton
-    private lateinit var prevButton: ImageButton
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var questionTextView: TextView
@@ -26,57 +28,110 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_asia, true)
     )
     private var currentIndex = 0
+    private var correctAnswers = 0
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d(TAG, "onCreate(Bundle?) called")
+
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
-        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
-            checkAnswer(true)
+            correctAnswers = if(checkAnswer(true))
+                correctAnswers + 1 else correctAnswers + 0
+            blockAnswerButons()
         }
         falseButton.setOnClickListener{view: View ->
-            checkAnswer(false)
+            correctAnswers = if(checkAnswer(false))
+                correctAnswers + 1 else correctAnswers + 0
+            blockAnswerButons()
         }
         nextButton.setOnClickListener { view: View ->
-            currentIndex = (currentIndex + 1) % questionBank.size
+            currentIndex++
+            if(currentIndex == questionBank.size){
+                showResult()
+                currentIndex = 0;
+            }
+            unblockAnswerButtons()
             updateQuestion()
         }
-        prevButton.setOnClickListener { view: View ->
-            currentIndex = currentIndex - 1
-            if(currentIndex == -1)
-                currentIndex = questionBank.size - 1
-            updateQuestion()
-        }
+
         questionTextView.setOnClickListener { view: View ->
-            currentIndex = (currentIndex + 1) % questionBank.size
+            currentIndex++
+            if(currentIndex == questionBank.size){
+                showResult()
+                currentIndex = 0;
+            }
+            unblockAnswerButtons()
             updateQuestion()
         }
-
-
         updateQuestion()
-    }
+    } // onCreate
+
+    override fun onStart(){
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    } // onStart
+
+    override fun onResume(){
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }// onResume
+
+    override fun onPause(){
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    } // onPause
+
+    override fun onStop(){
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    } // onStop
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
+    } // onDestroy
 
     private fun updateQuestion(){
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
     }
 
-    private fun checkAnswer(userAnswer: Boolean){
+    private fun checkAnswer(userAnswer: Boolean): Boolean{
+        val isCorrectAnswer: Boolean
         val correctAnswer = questionBank[currentIndex].answer
         val messageResId = if (userAnswer == correctAnswer){
+            isCorrectAnswer = true
             R.string.toast_correct
         } else {
+            isCorrectAnswer = false
             R.string.toast_incorrect
         }
         Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show()
+        return isCorrectAnswer
     }
 
+    private fun blockAnswerButons(){
+        trueButton.isEnabled = false
+        falseButton.isEnabled = false
+    }
+
+    private fun unblockAnswerButtons(){
+        trueButton.isEnabled = true
+        falseButton.isEnabled = true
+    }
+
+    private fun showResult(){
+        Toast.makeText(this,
+            "You answered ${correctAnswers * 100 / questionBank.size}%" +
+                    " of the questions correctly", Toast.LENGTH_LONG).show()
+    }
 
 }
